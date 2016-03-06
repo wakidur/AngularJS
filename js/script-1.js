684a674fdad40e3e901d6582a500e6cc6ab240f0
Wakidur probAZe <wakidur@gmail.com>
2016-03-06 16:30:31 +0600

    first file add

---
+++ js/script-1.js
@@ -0,0 +1,32 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+(function(angular) {
+  'use strict';
+angular.module('invoice1', [])
+  .controller('InvoiceController', function() {
+    this.qty = 1;
+    this.cost = 2;
+    this.inCurr = 'EUR';
+    this.currencies = ['USD', 'EUR', 'CNY','Bang'];
+    this.usdToForeignRates = {
+      USD: 1,
+      EUR: 0.74,
+      CNY: 6.09,
+      Bang: 80,
+    };
+
+    this.total = function total(outCurr) {
+      return this.convertCurrency(this.qty * this.cost, this.inCurr, outCurr);
+    };
+    this.convertCurrency = function convertCurrency(amount, inCurr, outCurr) {
+      return amount * this.usdToForeignRates[outCurr] / this.usdToForeignRates[inCurr];
+    };
+    this.pay = function pay() {
+      window.alert("Thanks!");
+    };
+  });
+})(window.angular);
