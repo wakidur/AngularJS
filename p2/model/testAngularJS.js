939bc46f44c57b9558d3ea875b245a330457f1c0
Wakidur probAZe <wakidur@gmail.com>
2016-04-07 18:37:24 +0600

    router file added

--- p2/model/testAngularJS.js
+++ p2/model/testAngularJS.js
@@ -5,5 +5,14 @@ var mainApp = angular.module("mainApp", []);
 var mainAppFilters = angular.module("mainAppFilters", []);
 var mainAppTable = angular.module("mainAppTable", []);
 var mainAppForm = angular.module("mainAppForm", []);
+var mainAppIncludes = angular.module("mainAppIncludes", []);
+
+
+
+ var shoppingCartModule = angular.module("shoppingCart", []);
+ var namesModule = angular.module("namesList", []);
+var AJAXfirst = angular.module("AJAXfirst", []);
+var AJAXsecond = angular.module('AJAXsecond', []);
+var myApplist = angular.module('myApplist', []);
 
 
