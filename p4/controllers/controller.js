d298fd94de1c143b5d7edced9688783832fa2945
Wakidur probAZe <wakidur@gmail.com>
2016-04-18 10:10:08 +0600

    p4 file add

---
+++ p4/controllers/controller.js
@@ -0,0 +1,21 @@
+/* 
+*17/04/2016
+ */
+//myMoudleApp.controller("moduleControll", function($scope){
+//    $scope.firstName = "Wakidur ";
+//    $scope.lastName = "Rahman";
+//});
+
+
+
+app.directive("w3TestDirective", function() {
+    return {
+        template : "I was made in a directive constructor!"
+    };
+});
+//creatNewDirrective.directive( "w3-test-directive", function() {
+//    return {
+//        template: "I was made in a directive constructor!"
+//    }    
+//})
+
---
+++ p4/modules/module.js
@@ -0,0 +1,8 @@
+/* 
+ * 17/04/2016
+ */
+//var myMoudleApp = angular.module( "modulApp" , []);
+var app = angular.module("myApp", []);
+//var creatNewDirrective = angular.module("Adding-Directive",[]);
+
+
