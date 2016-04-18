ff5ce4b824a4b349bad89d31fee717272c6679ee
Wakidur probAZe <wakidur@gmail.com>
2016-04-18 15:47:54 +0600

    some file add

--- p4/controllers/controller.js
+++ p4/controllers/controller.js
@@ -7,15 +7,36 @@
 //});
 
 
-
-app.directive("w3TestDirective", function() {
-    return {
-        template : "I was made in a directive constructor!"
-    };
-});
+/*-----------------------AngularJS Modules---------------------------*/
+//app.directive("w3TestDirective", function() {
+//    return {
+//        template : "I was made in a directive constructor!"
+//    };
+//});
 //creatNewDirrective.directive( "w3-test-directive", function() {
 //    return {
 //        template: "I was made in a directive constructor!"
 //    }    
 //})
+
+
+/*-----------------------AngularJS Directives---------------------*/
+elementApp.directive('w3TestDirective', function() {
+    return {
+        template: "<h1>Made by a directive!</h1>"
+    }
+});
+
+restrictionsApp.directive('RestrictionDirective' , function() {
+    return{
+        restrict : "E", // Attribute
+        template : "<h1>Restrictions for Element name</h1>"
+//        restrict : "A", // Attribute
+//        template : "<h1>Restrictions for Attribute</h1>"
+//        restrict : "C", // Attribute
+//        template : "<h1>Restrictions for Class</h1>"
+//        restrict : "M", // Attribute
+//        template : "<h1>Restrictions for Comment</h1>"
+    }
+})
 
--- p4/modules/module.js
+++ p4/modules/module.js
@@ -1,8 +1,31 @@
 /* 
  * 17/04/2016
  */
+
+/*-------------------AngularJS Modules-------------------------------*/
 //var myMoudleApp = angular.module( "modulApp" , []);
 var app = angular.module("myApp", []);
 //var creatNewDirrective = angular.module("Adding-Directive",[]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*------------------------AngularJS Directives-++++++++++++++++*/
+
+//create directive four way
+var elementApp = angular.module('elementApp',[]);
+// create directive and restrictions
+var restrictionsApp = angular.module("restrictionsApp", []);
+
 
 
