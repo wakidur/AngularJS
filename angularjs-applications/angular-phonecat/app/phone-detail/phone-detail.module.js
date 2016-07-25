65db758604ed8a4ce3d5f04c787cc617125de370
Wakidur probAZe <wakidur@gmail.com>
2016-07-25 11:42:59 +0600

    some file modified

--- angularjs-applications/angular-phonecat/app/phone-detail/phone-detail.module.js
+++ angularjs-applications/angular-phonecat/app/phone-detail/phone-detail.module.js
@@ -1,8 +1,13 @@
 (function (angular) {
     'use strict';
+<<<<<<< HEAD
+    // Define the `phoneDetail` module
+    angular.module('phoneDetail', ['ngRoute', 'corePhone']);
+
+=======
 
     // Define the `phoneDetail` module
     angular.module('phoneDetail', ['ngRoute', 'corePhone']);
 
-
+>>>>>>> 312f4add06738dd6c17afc16838feafc70183686
 })(window.angular);
